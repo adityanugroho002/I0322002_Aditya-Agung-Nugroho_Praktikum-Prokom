2f1d285975484b3c00717b38099873e5a2fce1dc
adityanugroho002 <adityanugroho@student.uns.ac.id>
2023-03-12 19:34:01 +0700

    Exercise 01

---
+++ Exercise-01.py
@@ -0,0 +1,15 @@
+print("""============================
+Nama : Aditya Agung Nugroho
+NIM  : I0322002
+============================
+    Saya tinggal di Bororejo, Baran, Cawas, Klaten. 
+Saya lahir dan juga dibesarkan di Klaten. Klaten 
+adalah kota yang berada di tengah-tengah Yogyakarta 
+dan Surakarta. Kampung halamanku merupakan daerah
+pedesaan yang masih banyak persawahan, kental akan
+budaya gotong royong. Jarak rumah ke UNS sekitar
+37 km yang bisa ditempuh dengan waktu 1 jam.
+    Harapan saya sebagai lulusan Teknik Industri
+yaitu dapat lulus dengan kemampuan yang mumpuni
+dan mendapatkan pekerjaan yang sesuai.
+============================""")
